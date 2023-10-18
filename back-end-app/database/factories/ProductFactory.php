1bb88f331223ca0d6f853b00cdd113779215c44e
Amin00sa <ami0190ne@gmail.com>
2023-10-18 16:41:23 +0100

    Factories && Seeders

---
+++ back-end-app/database/factories/ProductFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     */
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' => $this->faker->text,
+            'price' => $this->faker->randomFloat(0, 0, 9999999999.),
+            'image' => $this->faker->word,
+            'category_id' => Category::factory(),
+        ];
+    }
+}
